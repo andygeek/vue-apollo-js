e1ee95e3708da62ba52531f6a5023d8a68c2be7d
AndyGeek <andy.eulogio@gmail.com>
2020-10-30 14:01:45 -0500

    Graphql already done

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,30 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <div v-for="course in getCourses" :key="course.id">
+      {{ course.title }} -
+      {{ course.teacher }}
+    </div>
   </div>
 </template>
 
 <script>
+import gql from "graphql-tag";
 export default {
   name: "HelloWorld",
   props: {
     msg: String
+  },
+  apollo:{
+    getCourses: gql`
+      query {
+        getCourses {
+          _id
+          title
+          teacher
+        }
+      }
+    `
   }
 };
 </script>
